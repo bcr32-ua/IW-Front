3bf1e4e3a479636eeaadfc57c233e71d5a3b8f14
mgl120 <mgl120@alu.ua.es>
2025-01-16 22:52:47 +0100

    Contactanos con emailjs

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,100 +1,109 @@
 <template>
-    <div class="contact-page">
-      <NavBar />
-      <div class="background">
-        <div class="content">
-          <div class="contact-section">
-            <h2>Contáctanos</h2>
-            <div class="contact-info">
-              <p>
-                <strong>Teléfono:</strong> +34 123 456 789
-              </p>
-              <div class="map-container">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26142.943164140997!2d135.7493018756561!3d35.01001190278605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108a03887d72d%3A0xd92108a36694707!2sORIENTAL%20HOTEL%20KYOTO%20ROKUJO!5e0!3m2!1ses!2ses!4v1736964914887!5m2!1ses!2ses"
-                  width="100%"
-                  height="300"
-                  style="border:0;"
-                  allowfullscreen=""
-                  loading="lazy"
-                ></iframe>
-              </div>
+  <div class="contact-page">
+    <NavBar />
+    <div class="background">
+      <div class="content">
+        <div class="contact-section">
+          <h2>Contáctanos</h2>
+          <div class="contact-info">
+            <p>
+              <strong>Teléfono:</strong> +34 123 456 789
+            </p>
+            <div class="map-container">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26142.943164140997!2d135.7493018756561!3d35.01001190278605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108a03887d72d%3A0xd92108a36694707!2sORIENTAL%20HOTEL%20KYOTO%20ROKUJO!5e0!3m2!1ses!2ses!4v1736964914887!5m2!1ses!2ses"
+                width="100%"
+                height="300"
+                style="border:0;"
+                allowfullscreen=""
+                loading="lazy"
+              ></iframe>
             </div>
           </div>
-          <div class="suggestions-section">
-            <h2>Dudas o Sugerencias</h2>
-            <form @submit.prevent="sendMessage">
-              <div class="form-group">
-                <label for="email">Correo Electrónico</label>
-                <input
-                  type="email"
-                  id="email"
-                  v-model="formData.email"
-                  placeholder="nombre@ejemplo.com"
-                  required
-                />
-              </div>
-              <div class="form-group">
-                <label for="message">Mensaje</label>
-                <textarea
-                  id="message"
-                  v-model="formData.message"
-                  placeholder="Escribe tu mensaje aquí..."
-                  rows="5"
-                  required
-                ></textarea>
-              </div>
-              <button type="submit" class="submit-button">Enviar</button>
-            </form>
-            <div v-if="successMessage" class="success-message">
-              {{ successMessage }}
+        </div>
+        <div class="suggestions-section">
+          <h2>Dudas o Sugerencias</h2>
+          <form @submit.prevent="sendMessage">
+            <div class="form-group">
+              <label for="email">Correo Electrónico</label>
+              <input
+                type="email"
+                id="email"
+                v-model="formData.email"
+                placeholder="nombre@ejemplo.com"
+                required
+              />
             </div>
-            <div v-if="errorMessage" class="error-message">
-              {{ errorMessage }}
+            <div class="form-group">
+              <label for="message">Mensaje</label>
+              <textarea
+                id="message"
+                v-model="formData.message"
+                placeholder="Escribe tu mensaje aquí..."
+                rows="5"
+                required
+              ></textarea>
             </div>
+            <button type="submit" class="submit-button">Enviar</button>
+          </form>
+          <div v-if="successMessage" class="success-message">
+            {{ successMessage }}
+          </div>
+          <div v-if="errorMessage" class="error-message">
+            {{ errorMessage }}
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  import NavBar from "../components/NavBar.vue";
-  
-  export default {
-    components: {
-      NavBar,
-    },
-    data() {
-      return {
-        formData: {
-          email: "",
-          message: "",
-        },
-        successMessage: "",
-        errorMessage: "",
-      };
-    },
-    methods: {
-      async sendMessage() {
-        try {
-          const baseUrl = process.env.VUE_APP_URL_BACK;
-          await axios.post(`${baseUrl}/contact`, this.formData);
-          this.successMessage = "Mensaje enviado con éxito.";
-          this.errorMessage = "";
-          this.formData.email = "";
-          this.formData.message = "";
-        } catch (error) {
-          this.errorMessage =
-            error.response?.data || "Error al enviar el mensaje.";
-          this.successMessage = "";
-        }
+  </div>
+</template>
+
+<script>
+import emailjs from "emailjs-com";
+import NavBar from "../components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      formData: {
+        email: "",
+        message: "",
       },
+      successMessage: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    async sendMessage() {
+      try {
+        emailjs.send(
+          "service_kxsn51s", 
+          "template_hprhcrg", 
+          {
+            reply_to: this.formData.email, 
+            message: this.formData.message,
+          },
+          "1AdjvFsUH4gEiGzCE" 
+        );
+
+        this.successMessage = "Mensaje enviado con éxito.";
+        this.errorMessage = "";
+        this.formData.email = "";
+        this.formData.message = "";
+      } catch (error) {
+        console.error("Error al enviar el mensaje:", error);
+        this.errorMessage =
+          "Hubo un error al enviar el mensaje. Por favor, inténtalo nuevamente.";
+        this.successMessage = "";
+      }
     },
-  };
-  </script>
-  
+  },
+};
+</script>
+
   <style scoped>
   .contact-page {
     height: 100vh;
